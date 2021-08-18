7bb8db262a054e8c4219beae7f0d26dcf8d8eb79
anyexinglu <1171009543@qq.com>
2021-08-18 10:55:24 +0800

    根据教案优化代码（但缺点一样，文本节点需要完全是{{...}} 的格式）

--- mini-vue.js
+++ mini-vue.js
@@ -1,8 +1,9 @@
 export class MiniVue {
   constructor(config) {
     const { data, el, methods } = config;
+    this.effects = new Map();
     // data 做双向绑定，挂到 this 上
-    this.data = reactive(data);
+    this.data = reactive.bind(this)(data);
     this.template = document.querySelector(el);
     // 方法挂到 this 上
     Object.keys(methods).forEach(key => {
@@ -11,46 +12,42 @@ export class MiniVue {
       };
     });
     // template 内容替换
-    traverse(this.template, this);
+    this.traverse(this.template);
     console.log("...this", this);
   }
-}
 
-function traverse(dom, that) {
-  if (dom.nodeType === Node.TEXT_NODE) {
-    if (dom.data) {
-      dom.data = dom.data.replace(/{{(.*)}}/, ($0, $1) => {
-        const key = $1.trim();
-        if (!effects.get(key)) {
-          effects.set(key, []);
-        }
-        effects.get(key).push((value, oldValue) => {
-          console.log("....handler", value, oldValue);
-          dom.data = dom.data.replace(oldValue, value); // TODO hard code 需要改
-        });
-        return Reflect.get(that.data, key);
-      });
-    }
-  }
-  if (dom.attributes) {
-    for (let item of dom.attributes) {
-      let { name, value } = item;
-      if (name.startsWith("v-on")) {
-        let event = name.replace("v-on:", "");
-        dom.addEventListener(event, e => {
-          that[value](e);
+  traverse(dom) {
+    if (dom.nodeType === Node.TEXT_NODE) {
+      if (dom.data) {
+        dom.data = dom.data.replace(/{{(.*)}}/, ($0, $1) => {
+          const key = $1.trim();
+          if (!this.effects.get(key)) {
+            this.effects.set(key, []);
+          }
+          this.effects.get(key).push((value, oldValue) => {
+            console.log("....handler", value, oldValue);
+            dom.data = value; // TODO hard code 需要改
+          });
+          return Reflect.get(this.data, key);
         });
       }
     }
-  }
-  if (dom.childNodes) {
-    Array.from(dom.childNodes).forEach(child => {
-      traverse(child, that);
-    });
+    if (dom.attributes) {
+      for (let item of dom.attributes) {
+        let { name, value } = item;
+        if (name.startsWith("v-on")) {
+          let event = name.replace("v-on:", "");
+          dom.addEventListener(event, this[value]);
+        }
+      }
+    }
+    if (dom.childNodes) {
+      Array.from(dom.childNodes).forEach(child => {
+        this.traverse(child);
+      });
+    }
   }
 }
-
-let effects = new Map();
 
 function reactive(data) {
   return new Proxy(data, {
@@ -60,8 +57,8 @@ function reactive(data) {
     set: (obj, key, value) => {
       let oldValue = data[key];
       data[key] = value;
-      if (effects.get(key)) {
-        effects.get(key).forEach(handler => {
+      if (this.effects.get(key)) {
+        this.effects.get(key).forEach(handler => {
           handler(value, oldValue);
         });
       }
